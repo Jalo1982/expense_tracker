8c68cde947a98567d859f1a2e21a748bd4623060
ali.jalo <admirralty@gmail.com>
2024-07-29 11:25:14 +0300

    Active the cancel button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -47,7 +47,9 @@ class _NewExpenseState extends State<NewExpense> {
           Row(
             children: [
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pop(context);
+                },
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
